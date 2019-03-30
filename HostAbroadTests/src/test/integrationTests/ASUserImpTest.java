0a0ea170842a95a3f505976b23cdcb6ddc74005b
ivanalba1 <33236797+ivanalba1@users.noreply.github.com>
2019-03-30 12:33:13 +0100

    Pruebas funcionando

--- HostAbroadTests/src/test/integrationTests/ASUserImpTest.java
+++ HostAbroadTests/src/test/integrationTests/ASUserImpTest.java
@@ -15,19 +15,22 @@ public class ASUserImpTest {
 	public void loginUserTest() {
 		this.sauser = (ASUserImp) ASFactory.getInstance().createASUser();
 		
-		TUser original = new TUser("Adri", "Adrian", "adri@gmail.com", "adri1", 5, "pruebas para login", false, true);
+		//Necesario poner estos datos en tu bbdd antes de probar el test
+		TUser original = new TUser("Prueba", "PruebaFull", "ivan@ucm.es", "1234", 5, "pruebas para login", true, false);
 		TUser returned;
 		
 		returned = this.sauser.loginUser(original);
 		
-		assertEquals(original.getNickname(), returned.getNickname());
-		assertEquals(original.getFullName(), returned.getFullName());
-		assertEquals(original.getEmail(), returned.getEmail());
-		assertEquals(original.getPassword(), returned.getPassword());
-		assertEquals(original.getRating(), returned.getRating(), 2);
-		assertEquals(original.getDescription(), returned.getDescription());
-		assertEquals(original.getHost(), returned.getHost());
-		assertEquals(original.getTraveler(), returned.getTraveler());
+		if(returned != null) {
+			assertEquals(original.getNickname(), returned.getNickname());
+			assertEquals(original.getFullName(), returned.getFullName());
+			assertEquals(original.getEmail(), returned.getEmail());
+			assertEquals(original.getPassword(), returned.getPassword());
+			assertEquals(original.getRating(), returned.getRating(), 2);
+			assertEquals(original.getDescription(), returned.getDescription());
+			assertEquals(original.getHost(), returned.getHost());
+			assertEquals(original.getTraveler(), returned.getTraveler());
+		}
 	}
 
 }
